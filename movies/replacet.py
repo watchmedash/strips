0a6a9684d88deb912449d3b1a47676a0d916257b
watchmedash <mojazer2017@gmail.com>
2024-08-03 11:25:44 +0400

    up

--- movies/replacet.py
+++ movies/replacet.py
@@ -10,4 +10,4 @@ def replace_text_in_files(directory, old_text, new_text):
                     print(line.replace(old_text, new_text), end='')
 
 # Replace "Mercenary Enrollment" with "Overgeared" in all HTML files in the current directory
-replace_text_in_files('.', 'Dashflix', 'Nextflix')
+replace_text_in_files('.', 'https://i.postimg.cc/qMFTZzvm/logodf.webp', 'https://i.postimg.cc/Kv4RxRFw/logo1.png')
