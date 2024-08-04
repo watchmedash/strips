a93f323354acb3d5f51190f5a2c8cd9867b441c2
watchmedash <mojazer2017@gmail.com>
2024-08-04 09:59:18 +0400

    up

--- remove.py
+++ remove.py
@@ -1,10 +1,10 @@
 import os
 
 # Directory containing your HTML files
-directory = r'C:\Users\HP\Documents\GitHub\strips'  # Replace with your actual directory path
+directory = r'C:\Users\HP\Documents\GitHub\strips\movies'  # Replace with your actual directory path
 
 # Line to be removed
-line_to_remove = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2412399424552673"crossorigin="anonymous"></script>\n'
+line_to_remove = '<script async="" crossorigin="anonymous" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2412399424552673"></script>\n'
 
 def remove_link_tag(directory):
     for root, dirs, files in os.walk(directory):
